ffcdb1b2350283471197f5858b88ea06f22c8ef4
mandrabi1234 <mandrabi@oxy.edu>
2025-04-29 18:11:32 -0700

    Update factors_t20.py

--- Factor Calculations/factors_t20.py
+++ Factor Calculations/factors_t20.py
@@ -44,23 +44,20 @@ def opp_quality_factor(df, own_team_ranking, opposition_ranking, opp_quality_fac
     # Now clip on the Min/Max.
     df.loc[df[opp_quality_factor_col_name] < t20.OPP_QUALITY_FACTOR_MIN, opp_quality_factor_col_name] = t20.OPP_QUALITY_FACTOR_MIN
     df.loc[df[opp_quality_factor_col_name] > t20.OPP_QUALITY_FACTOR_MAX, opp_quality_factor_col_name] = t20.OPP_QUALITY_FACTOR_MAX
-'''
-4. If the code and everything above makes sense, you could try your hand at encoding another batting factor. 
-This could be the batting position factor where you could bucket batting positions in four buckets: POS 1-3, 
-POS 4-5, POS 6-8, POS 9-11. This factor could simply be 0.9 for runs made by POS 1-3, 1.0 for runs made by 4-5,
-1.1 for runs made by 6-8 and 1.2 for runs made by POS 9-11. Again, I am just using some starter factor constants
-here. The key would be to code this up in a way that the code doesn't need to change to update any of the 
-numbers here (factor values or position numbers).
-'''
+
 # Compute the batting position factor for each player (4 buckets- POS 1-3, 4-5, 6-8. 9-11)
 def batting_position_factor(df, runs_made_col_name, batting_position_col_name, bat_pos_factor_col_name):
     
+    # iterate through the dataframe, applying batting position multiplier to run values based on bucket
     for index in df.index:
             if 1 <= df.at[index, batting_position_col_name] <= 3:
                 df.at[index, bat_pos_factor_col_name] = (df.at[index, runs_made_col_name] * t20.POS_1_3)
+            
             if 4 <= df.at[index, batting_position_col_name] <= 5:
                 df.at[index, bat_pos_factor_col_name] = (df.at[index, runs_made_col_name] * t20.POS_4_5)
+            
             if 6 <= df.at[index, batting_position_col_name] <= 8:
                 df.at[index, bat_pos_factor_col_name] = (df.at[index, runs_made_col_name] * t20.POS_6_8)
+            
             if 9 <= df.at[index, batting_position_col_name] <= 11:
                 df.at[index, bat_pos_factor_col_name] = (df.at[index, runs_made_col_name] * t20.POS_9_11)
